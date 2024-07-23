0dc2b1bc7d8b54e2280acb3ecf5c5bd106a4d756
Edgar Mesquita <edgar@equantic.tech>
2024-07-23 15:10:49 +0100

    chore: Updating packages

--- src/Api.Crud/Options/AllCrudOptions.cs
+++ src/Api.Crud/Options/AllCrudOptions.cs
@@ -39,6 +39,11 @@ public class AllCrudOptions
     public EntityCrudOptions<TEntity1, TEntity2, TEntity3> For<TEntity1, TEntity2, TEntity3>() => new(this);
     public EntityCrudOptions<TEntity1, TEntity2, TEntity3, TEntity4> For<TEntity1, TEntity2, TEntity3, TEntity4>() => new(this);
     public EntityCrudOptions<TEntity1, TEntity2, TEntity3, TEntity4, TEntity5> For<TEntity1, TEntity2, TEntity3, TEntity4, TEntity5>() => new(this);
+    public EntityCrudOptions<TEntity1, TEntity2, TEntity3, TEntity4, TEntity5, TEntity6> For<TEntity1, TEntity2, TEntity3, TEntity4, TEntity5, TEntity6>() => new(this);
+    public EntityCrudOptions<TEntity1, TEntity2, TEntity3, TEntity4, TEntity5, TEntity6, TEntity7> For<TEntity1, TEntity2, TEntity3, TEntity4, TEntity5, TEntity6, TEntity7>() => new(this);
+    public EntityCrudOptions<TEntity1, TEntity2, TEntity3, TEntity4, TEntity5, TEntity6, TEntity7, TEntity8> For<TEntity1, TEntity2, TEntity3, TEntity4, TEntity5, TEntity6, TEntity7, TEntity8>() => new(this);
+    public EntityCrudOptions<TEntity1, TEntity2, TEntity3, TEntity4, TEntity5, TEntity6, TEntity7, TEntity8, TEntity9> For<TEntity1, TEntity2, TEntity3, TEntity4, TEntity5, TEntity6, TEntity7, TEntity8, TEntity9>() => new(this);
+    public EntityCrudOptions<TEntity1, TEntity2, TEntity3, TEntity4, TEntity5, TEntity6, TEntity7, TEntity8, TEntity9, TEntity10> For<TEntity1, TEntity2, TEntity3, TEntity4, TEntity5, TEntity6, TEntity7, TEntity8, TEntity9, TEntity10>() => new(this);
 
     internal Assembly? GetAssembly() => _assembly;
     internal Dictionary<Type, Action<ICrudOptions>> GetOptions() => _options;
@@ -97,6 +102,86 @@ public class AllCrudOptions
             allCrudOptions._options.Add(typeof(TEntity3), options);
             allCrudOptions._options.Add(typeof(TEntity4), options);
             allCrudOptions._options.Add(typeof(TEntity5), options);
+            return allCrudOptions;
+        }
+    }
+    
+    public class EntityCrudOptions<TEntity1, TEntity2, TEntity3, TEntity4, TEntity5, TEntity6>(AllCrudOptions allCrudOptions)
+    {
+        public AllCrudOptions UseOptions(Action<ICrudOptions> options)
+        {
+            allCrudOptions._options.Add(typeof(TEntity1), options);
+            allCrudOptions._options.Add(typeof(TEntity2), options);
+            allCrudOptions._options.Add(typeof(TEntity3), options);
+            allCrudOptions._options.Add(typeof(TEntity4), options);
+            allCrudOptions._options.Add(typeof(TEntity5), options);
+            allCrudOptions._options.Add(typeof(TEntity6), options);
+            return allCrudOptions;
+        }
+    }
+    
+    public class EntityCrudOptions<TEntity1, TEntity2, TEntity3, TEntity4, TEntity5, TEntity6, TEntity7>(AllCrudOptions allCrudOptions)
+    {
+        public AllCrudOptions UseOptions(Action<ICrudOptions> options)
+        {
+            allCrudOptions._options.Add(typeof(TEntity1), options);
+            allCrudOptions._options.Add(typeof(TEntity2), options);
+            allCrudOptions._options.Add(typeof(TEntity3), options);
+            allCrudOptions._options.Add(typeof(TEntity4), options);
+            allCrudOptions._options.Add(typeof(TEntity5), options);
+            allCrudOptions._options.Add(typeof(TEntity6), options);
+            allCrudOptions._options.Add(typeof(TEntity7), options);
+            return allCrudOptions;
+        }
+    }
+    
+    public class EntityCrudOptions<TEntity1, TEntity2, TEntity3, TEntity4, TEntity5, TEntity6, TEntity7, TEntity8>(AllCrudOptions allCrudOptions)
+    {
+        public AllCrudOptions UseOptions(Action<ICrudOptions> options)
+        {
+            allCrudOptions._options.Add(typeof(TEntity1), options);
+            allCrudOptions._options.Add(typeof(TEntity2), options);
+            allCrudOptions._options.Add(typeof(TEntity3), options);
+            allCrudOptions._options.Add(typeof(TEntity4), options);
+            allCrudOptions._options.Add(typeof(TEntity5), options);
+            allCrudOptions._options.Add(typeof(TEntity6), options);
+            allCrudOptions._options.Add(typeof(TEntity7), options);
+            allCrudOptions._options.Add(typeof(TEntity8), options);
+            return allCrudOptions;
+        }
+    }
+    
+    public class EntityCrudOptions<TEntity1, TEntity2, TEntity3, TEntity4, TEntity5, TEntity6, TEntity7, TEntity8, TEntity9>(AllCrudOptions allCrudOptions)
+    {
+        public AllCrudOptions UseOptions(Action<ICrudOptions> options)
+        {
+            allCrudOptions._options.Add(typeof(TEntity1), options);
+            allCrudOptions._options.Add(typeof(TEntity2), options);
+            allCrudOptions._options.Add(typeof(TEntity3), options);
+            allCrudOptions._options.Add(typeof(TEntity4), options);
+            allCrudOptions._options.Add(typeof(TEntity5), options);
+            allCrudOptions._options.Add(typeof(TEntity6), options);
+            allCrudOptions._options.Add(typeof(TEntity7), options);
+            allCrudOptions._options.Add(typeof(TEntity8), options);
+            allCrudOptions._options.Add(typeof(TEntity9), options);
+            return allCrudOptions;
+        }
+    }
+    
+    public class EntityCrudOptions<TEntity1, TEntity2, TEntity3, TEntity4, TEntity5, TEntity6, TEntity7, TEntity8, TEntity9, TEntity10>(AllCrudOptions allCrudOptions)
+    {
+        public AllCrudOptions UseOptions(Action<ICrudOptions> options)
+        {
+            allCrudOptions._options.Add(typeof(TEntity1), options);
+            allCrudOptions._options.Add(typeof(TEntity2), options);
+            allCrudOptions._options.Add(typeof(TEntity3), options);
+            allCrudOptions._options.Add(typeof(TEntity4), options);
+            allCrudOptions._options.Add(typeof(TEntity5), options);
+            allCrudOptions._options.Add(typeof(TEntity6), options);
+            allCrudOptions._options.Add(typeof(TEntity7), options);
+            allCrudOptions._options.Add(typeof(TEntity8), options);
+            allCrudOptions._options.Add(typeof(TEntity9), options);
+            allCrudOptions._options.Add(typeof(TEntity10), options);
             return allCrudOptions;
         }
     }
